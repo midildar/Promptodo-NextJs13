d8d5632c3698e95c52ed9c4906557ded7ad8f1aa
Ismaeel <ismaeeldildar.98@gmail.com>
2023-05-20 21:58:19 +0500

    data leak fixed

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -17,7 +17,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
     setTimeout(() => setiscopied(""), 3000);
   };
   const handleProfileClick = () =>{
-    console.log(post)
+
     if (post.creator._id === session?.user.id) return router.push("/profile")
     router.push(`/profile/${post.creator._id}?name=${post.creator.username}`)
   }
